719b1aea5fd9b84e6fdd169c0f69a798b44ff095
Aylin210302 <aylinorozco856@gmail.com>
2021-09-24 13:41:55 -0600

    Crud

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: 'https://catalogo-producto-b524b-default-rtdb.firebaseio.com/'
 };
 
 /*
